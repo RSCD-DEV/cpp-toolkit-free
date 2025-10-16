b15851980fa8e22b13754463ae308d60c2282e3f
Robert Schneider <info@rscd.net>
2025-10-16 12:06:37 +0200

    Rename Entry Point File

--- src/extensionEntry.ts
+++ src/extensionEntry.ts
@@ -2,10 +2,8 @@ import * as vscode from 'vscode';
 import { registerUnusedVarChecker } from './unusedVarChecker/unusedVarManager';
 import { registerUnusedIncludeChecker } from './unusedIncludeChecker/unusedIncludeManager';
 
-export function activate(context: vscode.ExtensionContext) {
+export function registerFeatures(context: vscode.ExtensionContext) {
 	console.log("Starting extension...");
 	registerUnusedVarChecker(context);
 	registerUnusedIncludeChecker(context);
 }
-
-export function deactivate() { }
